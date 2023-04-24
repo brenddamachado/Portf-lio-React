b232a6403d0b3a291eec34ee5809698879254e1e
brenddamachado <brenda16machado.bm@gmail.com>
2023-04-23 22:37:11 -0300

    img, components

--- portfolio_react/src/App.jsx
+++ portfolio_react/src/App.jsx
@@ -1,10 +1,16 @@
-
+import { MainContent } from './components/MainContent'
+import { Sidebar } from './components/Sidebar'
+import './styles/components/App.sass'
 
 function App() {
-  
+
 
   return (
-  <h1>reacrt</h1>
+    <div id="porfolio">
+      <h1>Brenda Machado</h1>
+      <Sidebar />
+      <MainContent />
+    </div>
   )
 }
 
