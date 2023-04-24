7ca97919325a447575526a72edd00af418fa4517
brenddamachado <brenda16machado.bm@gmail.com>
2023-04-24 00:12:54 -0300

    atualizando

--- portfolio_react/src/components/Sidebar.jsx
+++ portfolio_react/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import foto from '../img/BrendaMachado.jpg'
+import foto from '../img/eu.jfif'
 import '../styles/components/sidebar.sass'
 export const Sidebar = () => {
   return <aside id='sidebar'>
