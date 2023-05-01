f5596c9be80f390c115c9b3b22a9fdaf01dc9ca7
brenddamachado <brenda16machado.bm@gmail.com>
2023-04-30 23:24:47 -0300

    icons

--- portfolio_react/src/components/Sidebar.jsx
+++ portfolio_react/src/components/Sidebar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import foto from '../img/eu.jfif'
 import '../styles/components/sidebar.sass'
+import SocialNetworks from './SocialNetworks'
 export const Sidebar = () => {
   return <aside id='sidebar'>
    <img src={foto} alt="Brenda Machado" />
 
     <p className="title">Desenvolvedora </p>
-    <p>redes sociais</p>
+   <SocialNetworks />
     <p>informaçoes de contato</p>
     <a href="" className='btn'>
       download curriculo
